5e41c279d7c51ef26dc87a48c1b6c897207096ae
ProfessorDy <kingdeeyfx@gmail.com>
2023-08-30 15:11:23 +0100

    Complete headphones page

--- app/headphones/page.tsx
+++ app/headphones/page.tsx
@@ -1,9 +1,15 @@
 import ImageLinks from "@/components/ImageLinks"
 import Product from "@/components/Product"
 import SubHero from "@/components/SubHero"
-import imgMobile from '@/public/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg'
-import imgTablet from '@/public/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg'
-import imgDesktop from '@/public/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg'
+import mark2Mobile from '@/public/product-xx99-mark-two-headphones/mobile/image-category-page-preview.jpg'
+import mark2Tablet from '@/public/product-xx99-mark-two-headphones/tablet/image-category-page-preview.jpg'
+import mark2Desktop from '@/public/product-xx99-mark-two-headphones/desktop/image-category-page-preview.jpg'
+import mark1Mobile from '@/public/product-xx99-mark-one-headphones/mobile/image-category-page-preview.jpg'
+import mark1Tablet from '@/public/product-xx99-mark-one-headphones/tablet/image-category-page-preview.jpg'
+import mark1Desktop from '@/public/product-xx99-mark-one-headphones/desktop/image-category-page-preview.jpg'
+import xx59Mobile from '@/public/product-xx59-headphones/mobile/image-category-page-preview.jpg'
+import xx59Tablet from '@/public/product-xx59-headphones/tablet/image-category-page-preview.jpg'
+import xx59Desktop from '@/public/product-xx59-headphones/desktop/image-category-page-preview.jpg'
 
 
 
@@ -13,8 +19,12 @@ export default function Headphones() {
         <>
             <SubHero />
             <main className="padding-x">
-                <div className="wrapper">
-                    <Product imageMobile={imgMobile} imageTablet={imgTablet} imageDesktop={imgDesktop} name="xx99 mark ii" details="The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound." route="" invert={false} newProduct={true} />
+                <div className="wrapper space-y-[7.5rem] lg:space-y-40">
+                    <Product imageMobile={mark2Mobile} imageTablet={mark2Tablet} imageDesktop={mark2Desktop} name="xx99 mark ii" details="The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound." route="xx99-mark-two" invert={false} newProduct={true} />
+                    <Product imageMobile={mark1Mobile} imageTablet={mark1Tablet} imageDesktop={mark1Desktop} name="xx99 mark ii" details="The new XX99 Mark II headphones is the pinnacle of pristine audio. It redefines your premium headphone experience by reproducing the balanced depth and precision of studio-quality sound." route="xx99-mark-one" invert={true} newProduct={false} />
+
+                    <Product imageMobile={xx59Mobile} imageTablet={xx59Tablet} imageDesktop={xx59Desktop} name="xx59" details="Enjoy your audio almost anywhere and customize it to your specific tastes with the XX59 headphones. The stylish yet durable versatile wireless headset is a brilliant companion at home or on the move." route="xx59" invert={false} newProduct={false} />
+
                 </div>
                 <ImageLinks />
             </main>
--- components/Product.tsx
+++ components/Product.tsx
@@ -9,7 +9,7 @@ export default function Product({ imageMobile, imageTablet, imageDesktop, name, 
     const currentRoute: string = usePathname()
 
     return (
-        <section className={`flex flex-col gap-8 ${invert ? 'lg:flex-row-reverse' : 'lg:flex-row'} lg:items-center lg:gap-[7.81rem]`}>
+        <section className={`flex flex-col gap-8 sm:gap-[3.25rem] ${invert ? 'lg:flex-row-reverse' : 'lg:flex-row'} lg:items-center lg:gap-[7.81rem]`}>
             <Image
                 src={imageMobile}
                 alt={`${name} ${currentRoute.slice(1)}`}
@@ -23,16 +23,16 @@ export default function Product({ imageMobile, imageTablet, imageDesktop, name, 
             <Image
                 src={imageDesktop}
                 alt={`${name} ${currentRoute.slice(1)}`}
-                className="rounded-lg max-lg:hidden"
+                className="rounded-lg max-lg:hidden w-1/2"
             />
 
 
 
-            <div className="max-lg:text-center space-y-6">
-                <p className="text-primary text-[.875rem] tracking-[.625rem] uppercase font-normal">{newProduct ? 'new product' : ''}</p>
-                <h2 className="text-black text-[1.75rem] font-bold tracking-[.0625rem] uppercase  ">{name}<br />{currentRoute.slice(1)}</h2>
-                <p className="text-black text-[.9375rem] font-medium leading-[1.5625] opacity-50 ">{details}</p>
-                <Link href={`${currentRoute}/${route}`} className='primary-btn max-lg:mx-auto'>see product</Link>
+            <div className="max-lg:text-center max-sm:space-y-6 sm:max-w-[35.75rem] mx-auto">
+                <p className="text-primary text-[.875rem] tracking-[.625rem] uppercase font-normal sm:mb-4">{newProduct ? 'new product' : ''}</p>
+                <h2 className="text-black text-[1.75rem] sm:text-[2.5rem] sm:leading-[2.75rem] font-bold tracking-[.0625rem] sm:tracking-[0.08931rem] uppercase ">{name}<br />{currentRoute.slice(1)}</h2>
+                <p className="text-black text-[.9375rem] font-medium leading-[1.5625rem ] opacity-50 sm:mt-8">{details}</p>
+                <Link href={`${currentRoute}/${route}`} className='primary-btn max-lg:mx-auto sm:mt-8 lg:mt-10'>see product</Link>
             </div>
         </section>
     )
