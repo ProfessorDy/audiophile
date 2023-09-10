98ccbe3f4c0b9490c6f00f62529a13f8a809af37
ProfessorDy <kingdeeyfx@gmail.com>
2023-09-10 21:31:21 +0100

    Close modal on backdrop click

--- components/Dialog.tsx
+++ components/Dialog.tsx
@@ -1,14 +1,28 @@
 "use client"
-import { useSearchParams } from "next/navigation"
+import { useSearchParams, usePathname } from "next/navigation"
+import { useRouter } from "next/navigation"
 import { useRef, useEffect } from "react"
+
 
 
 
 export default function Dialog() {
 
+    const router = useRouter()
+    const currentRoute = usePathname()
     const searchParams = useSearchParams()
     const dialogRef = useRef<null | HTMLDialogElement>(null)
     const showDialog = searchParams.get("showDialog")
+
+    const handleClick = (event: any) => {
+        event.preventDefault();
+        if (event.target.nodeName === 'DIALOG') {
+            dialogRef.current?.close()
+            router.replace(currentRoute, { scroll: false });
+
+
+        }
+    }
 
     useEffect(() => {
         showDialog === 'y' ? dialogRef.current?.showModal() : dialogRef.current?.close()
@@ -17,24 +31,24 @@ export default function Dialog() {
 
     const dialog: JSX.Element | null = showDialog === 'y'
         ? (
-            <div className="relative">
-                <dialog ref={dialogRef} className="backdrop:bg-black/40 rounded-lg wrapper w-full max-w-[23.5625rem] sm:ml-auto bg-white py-8 px-7 space-y-8">
-                    <header className="flex justify-between items-center">
-                        <h1 className="uppercase text-[1.125rem] font-bold tracking-[0.08038rem] ">cart (3)</h1>
-                        <button className="text-[.9375rem] font-medium leading-[1.5625rem] underline opacity-50">Remove all</button>
-                    </header>
-                    <section className="space-y-6">
-                    </section>
-                    <footer className="space-y-6">
-                        <div className="flex justify-between items-center">
-                            <p className="text-[.9375rem] font-medium leading-[1.5625rem] opacity-50 uppercase">total </p>
-                            <p className="text-[1.125rem] font-bold">price</p>
-                        </div>
-                        <button className="w-full primary-btn"
-                        >Checkout</button>
-                    </footer>
-                </dialog>
-            </div>
+
+            <dialog onClick={handleClick} ref={dialogRef} className="backdrop:bg-black/40 rounded-lg sm:wrapper w-full max-w-[23.5625rem] sm:w-[23.5625rem] sm:translate-x-[50] bg-white py-8 px-7 space-y-8 ">
+                <header className="flex justify-between items-center">
+                    <h1 className="uppercase text-[1.125rem] font-bold tracking-[0.08038rem] ">cart (3)</h1>
+                    <button className="text-[.9375rem] font-medium leading-[1.5625rem] underline opacity-50">Remove all</button>
+                </header>
+                <section className="space-y-6">
+                </section>
+                <footer className="space-y-6">
+                    <div className="flex justify-between items-center">
+                        <p className="text-[.9375rem] font-medium leading-[1.5625rem] opacity-50 uppercase">total </p>
+                        <p className="text-[1.125rem] font-bold">price</p>
+                    </div>
+                    <button className="w-full primary-btn"
+                    >Checkout</button>
+                </footer>
+            </dialog>
+
         )
         : null
 
