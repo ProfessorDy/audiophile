233657e727f1f0d9277a82bb36fe89c433e51b71
ProfessorDy <kingdeeyfx@gmail.com>
2023-09-11 17:24:50 +0100

    Adjust hero padding

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -7,8 +7,8 @@ import Link from 'next/link'
 export default function Hero() {
     return (
         <>
-            <div className="bg-hero w-full">
-                <section className="bg-hero-mobile bg-cover sm:bg-hero-tablet lg:bg-hero-desktop sm:bg-cover lg:bg-left-top bg-no-repeat  lg:bg-contain max-lg:padding-x py-[6.75rem] sm:py-[7.88rem]  lg:pt-24 lg:pb-[9.88rem] wrapper max-lg:text-center text-white uppercase">
+            <div className="bg-hero w-full padding-x">
+                <section className="bg-hero-mobile bg-cover sm:bg-hero-tablet lg:bg-hero-desktop sm:bg-cover lg:bg-left-top bg-no-repeat  lg:bg-contain  py-[6.75rem] sm:py-[7.88rem]  lg:pt-24 lg:pb-[9.88rem] wrapper max-lg:text-center text-white uppercase">
                     <p className="text-[0.875rem] font-normal tracking-[0.625rem] opacity-[0.4964] ">new product</p>
                     <h1 className="text-4xl sm:text-[3.5rem] sm:leading-[3.625rem] font-bold tracking-[0.08038rem] sm:tracking-[0.125rem] mt-4 sm:mt-6 mx-auto lg:mx-0">xx99 mark ii<br />headphones</h1>
                     <p className="normal-case text-[0.9375rem] font-medium leading-[1.5625rem] opacity-75 mt-6  sm:mx-auto lg:mx-0">
